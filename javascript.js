49a869de785b41d734ac02ef7d8e0edf039bbe9b
cheventner <va.cheventner@gmail.com>
2019-10-07 16:34:11 +0200

    sauvegarde071019

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function conversion(taux){
+	var valeur = prompt("entrer valeur en euro");
+	var resultat = valeur * taux;
+	alert("Valeur :" +valeur + "\nRésultat : " +resultat);
+}
+	conversion();
+	console.log(resultat);
